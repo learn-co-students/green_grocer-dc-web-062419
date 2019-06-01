785f27c80daf340bb058ea3269485cf84abaa47d
Trevahr Robertson <trevahr@yahoo.com>
2019-06-01 21:24:03 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,24 @@
 def consolidate_cart(cart)
-  # code here
+	i = 0
+	s = {}
+	cart.each do |item|
+		item.map do |name, info|
+		s[name] = info
+		s[name][:count] = 0
+		
+		cur = name
+	cart.each do |item|
+		item.map do |name2, info2|
+			if name2 == cur
+				s[name][:count] += 1
+			end
+			
+	end
+	end
+end
+i += 1
+end.uniq
+s
 end
 
 def apply_coupons(cart, coupons)
@@ -13,3 +32,4 @@ end
 def checkout(cart, coupons)
   # code here
 end
+
