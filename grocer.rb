3fc083eb8c62a067a4771fa4699f46cf34c1661f
Trevahr Robertson <trevahr@yahoo.com>
2019-06-11 18:16:59 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -2,17 +2,15 @@ def consolidate_cart(cart)
 	i = 0
 	s = {}
 	cart.each do |item|
-		item.map do |name, info|
-		s[name] = info
+	  item.map do |name, info|
+		  s[name] = info
 		s[name][:count] = 0
-		
 		cur = name
 	cart.each do |item|
 		item.map do |name2, info2|
 			if name2 == cur
 				s[name][:count] += 1
 			end
-			
 	end
 	end
 end
@@ -21,12 +19,31 @@ end.uniq
 s
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+    if cart[name] && cart[name][:count] >= coupon[:num]
+      if cart["#{name} W/COUPON"]
+        cart["#{name} W/COUPON"][:count] += 1
+      else
+        cart["#{name} W/COUPON"] = {:count => 1, :price => coupon[:cost], :clearance => cart[name][:clearance]}
+      end
+      cart[name][:count] -= coupon[:num]
+    end
+  end
+  cart
+
 end
+
 
 def apply_clearance(cart)
   # code here
+	cart.each do |item, details|
+		if  details[:clearance] == true
+			details[:price] = (details[:price]*0.80).round(2)
+		end
+	end
 end
 
 def checkout(cart, coupons)
